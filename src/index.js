333ba955892ef63f563909388762d356d3c5aba2
Ivan Miropolskii <miropolskiy.ivan@gmail.com>
2024-07-29 21:15:36 -0500

    feat: add toReadable func logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const BELOW_TWENTY = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'
+    ];
+    const TENS = [
+        '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+
+    switch (true) {
+        case number < 20:
+            return BELOW_TWENTY[number];
+        case number < 100:
+            const ten = Math.floor(number / 10);
+            const unit = number % 10;
+            return unit === 0 ? TENS[ten] : `${TENS[ten]} ${BELOW_TWENTY[unit]}`;
+        case number < 1000:
+            const hundred = Math.floor(number / 100);
+            const rest  = number % 100;
+            const restReadable = rest ? ` ${toReadable(rest)}` : '';
+            return `${BELOW_TWENTY[hundred]} hundred${restReadable}`;
+        default:
+            return 'Out of range';
+    }
 }
